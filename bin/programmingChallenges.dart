e4bde550c24aba43655034b808bdc9f968244bd8
yasmin <montageyasmin2003@gmail.com>
2020-06-08 15:47:58 -0700

    my comment

--- bin/programmingChallenges.dart
+++ bin/programmingChallenges.dart
@@ -2,8 +2,8 @@
 //import 'package:programmingChallenges/challenge_002.dart';
 //import 'package:programmingChallenges/challenge_003.dart';
 //import 'package:programmingChallenges/challenge_004.dart';
-//import 'package:programmingChallenges/challenge_005.dart';
+import 'package:programmingChallenges/challenge_005.dart';
 void main() {
-//runProgram();
+runProgram();
   //5 or 101
 }
--- lib/challenge_005.dart
+++ lib/challenge_005.dart
@@ -1,3 +1,23 @@
-void runProgram(){
+import 'dart:io';
 
+int getYearOfBirth(){
+  print('Enter year');
+  return int.parse(stdin.readLineSync());
+}
+int getMonthOfBirth(){
+  print('Enter month');
+  return int.parse(stdin.readLineSync());
+}
+int getDayOfBirth(){
+  print('Enter day of birth');
+  return int.parse(stdin.readLineSync());
+}
+
+void runProgram(){
+  var dataFrom = DateTime.utc(getYearOfBirth(),getMonthOfBirth(),getDayOfBirth());
+  var dataTo = DateTime.now().toUtc();
+  var difference =dataTo.difference(dataFrom);
+  var result = difference.inDays;
+
+  print('you have lived for $result days ');
 }
